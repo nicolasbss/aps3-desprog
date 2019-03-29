2274133f7f905b81f3e48ce1ead4b4369c4ad099
Guilherme Silvarolli Aliperti <gui.aliperti9@gmail.com>
2019-03-29 14:24:06 -0300

    finalizado

--- dna.c
+++ dna.c
@@ -18,45 +18,68 @@ void load_string(FILE *file, int *p, char *s, int t) {
 }
 
 int mlcs_w(char a[], int n, char b[], int m, int length[MAX_SIZE + 1][MAX_SIZE + 1]) {
-	if (length[n][m] != -1) {
-		return length[n][m];
-	}
-	if (m == 0 || n == 0) {
-		length[n][m] = 0;
-		return length[n][m];
-	}
-	if (a[n-1] == b[m-1]) {
-		length[n][m] = mlcs_w(a, n-1, b, m-1, length) + 1;
-	}
-	else {
-		length[n-1][m] = mlcs_w(a, n - 1, b, m, length);
-		length[n][m-1] = mlcs_w(a, n, b, m - 1, length);
+  //checa se essa posição da matriz já foi calculada
+  if (length[n][m] != -1) {
+    return length[n][m];
+  }
+  //condição da base da recursão
+  if (m == 0 || n == 0) {
+    length[n][m] = 0;
+    return length[n][m];
+  }
+  //condição do passo da recursão
+  if (a[n - 1] == b[m - 1]) {
+    length[n][m] = mlcs_w(a, n - 1, b, m - 1, length) + 1;
+  } else {
+    length[n - 1][m] = mlcs_w(a, n - 1, b, m, length);
+    length[n][m - 1] = mlcs_w(a, n, b, m - 1, length);
 
-		if (length[n-1][m] > length[n][m-1]) {
-			length[n][m] = length[n-1][m];
-		} else {
-			length[n][m] = length[n][m-1];
-		}
-
-	}
+    if (length[n - 1][m] > length[n][m - 1]) {
+      length[n][m] = length[n - 1][m];
+    } else {
+      length[n][m] = length[n][m - 1];
+    }
+  }
 
   return length[n][m];
 }
 
 int mlcs(char a[], int n, char b[], int m) {
-	int length[MAX_SIZE + 1][MAX_SIZE + 1];
-
- 	for (int j = 0; j < MAX_SIZE + 1; j++) {
-    		for (int i = 0; i < MAX_SIZE + 1; i++) {
-      		length[i][j] = -1;
-    		}
- 	}
+  //inicializa a matriz
+  int length[MAX_SIZE + 1][MAX_SIZE + 1];
+  //preenche a matriz com -1
+  for (int j = 0; j <= n; j++) {
+    for (int i = 0; i <= m; i++) {
+      length[j][i] = -1;
+    }
+  }
   length[0][0] = 0;
   mlcs_w(a, n, b, m, length);
-	return length[n][m];
- 
+  return length[n][m];
 }
 
 int dlcs(char a[], int n, char b[], int m) {
-  return 2;
+  int length[MAX_SIZE + 1][MAX_SIZE + 1];
+
+//loop iterativo para percorrer todos os indices
+  for (int j = 0; j <= n; j++) {
+    for (int i = 0; i <= m; i++) {
+      //caso 1
+      if (j == 0 || i == 0) {
+        length[j][i] = 0;
+        //caso 2
+      } else if (a[j - 1] == b[i - 1]) {
+        length[j][i] = length[j - 1][i - 1] + 1;
+        //caso 3: verifica qual é maior
+      } else {
+        if (length[j - 1][i] >= length[j][i - 1]) {
+          length[j][i] = length[j - 1][i];
+        } else {
+          length[j][i] = length[j][i - 1];
+        }
+      }
+    }
+  }
+
+  return length[n][m];
 }
